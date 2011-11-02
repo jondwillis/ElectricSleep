f23359b63d454314f142fc9066f28bccc63eb900
Jon Willis <jondwillis@gmail.com>
2011-11-01 23:27:02 -0100

    not much really

--- src/com/androsz/electricsleepbeta/preference/CustomTitlebarPreferenceActivity.java
+++ src/com/androsz/electricsleepbeta/preference/CustomTitlebarPreferenceActivity.java
@@ -19,24 +19,6 @@ public abstract class CustomTitlebarPreferenceActivity extends PreferenceActivit
 
 	protected abstract String getPreferencesName();
 
-	public void hideTitleButton1() {
-		/*
-		 * final ImageButton btn1 = (ImageButton)
-		 * findViewById(R.id.title_button_1);
-		 * btn1.setVisibility(View.INVISIBLE);
-		 * findViewById(R.id.title_sep_1).setVisibility(View.INVISIBLE);
-		 */
-	}
-
-	public void hideTitleButton2() {
-		/*
-		 * final ImageButton btn2 = (ImageButton)
-		 * findViewById(R.id.title_button_2);
-		 * btn2.setVisibility(View.INVISIBLE);
-		 * findViewById(R.id.title_sep_2).setVisibility(View.INVISIBLE);
-		 */
-	}
-
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 
@@ -57,18 +39,6 @@ public abstract class CustomTitlebarPreferenceActivity extends PreferenceActivit
 		// getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
 		// R.layout.titlebar);
 		// ((TextView) findViewById(R.id.title_text)).setText(getTitle());
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(final Menu menu) {
-		// final MenuInflater inflater = getMenuInflater();
-		// inflater.inflate(R.menu.titlebar_menu, menu);
-		return false;
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
 	}
 
 	public void onClick(final View v) {
