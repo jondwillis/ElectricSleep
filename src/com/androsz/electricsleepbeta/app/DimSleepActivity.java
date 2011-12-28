db719712c7925aec00f25fc68bf74031114c1bf6
jondwillis <jondwillis@gmail.com>
2011-12-28 01:32:02 -0500

    Dim sleep: any user interaction causes finish(), hides system bar, actionbar and begins low key mode if the device supports it.

--- src/com/androsz/electricsleepbeta/app/DimSleepActivity.java
+++ src/com/androsz/electricsleepbeta/app/DimSleepActivity.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
@@ -20,7 +22,6 @@ public class DimSleepActivity extends AnalyticActivity {
 		final Window win = getWindow();
 		final WindowManager.LayoutParams winParams = win.getAttributes();
 		winParams.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-		// | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
 				| WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 		// set screen brightness to the lowest possible without turning screen
@@ -40,27 +41,44 @@ public class DimSleepActivity extends AnalyticActivity {
 			winParams.buttonBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_OFF;
 		}
 
-		// win.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		win.setAttributes(winParams);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+		getSupportActionBar().setDisplayShowHomeEnabled(false);
+		getSupportActionBar().setDisplayShowTitleEnabled(false);
+		
 		final LinearLayout blackness = new LinearLayout(this);
 		blackness.setLayoutParams(new LayoutParams(
 				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
 				android.view.ViewGroup.LayoutParams.MATCH_PARENT));
 
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			blackness.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+		} else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+			blackness.setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+		}
+
 		blackness.setBackgroundColor(Color.BLACK);
-		Toast.makeText(this, "Your screen is in dim mode. To exit, press the back button.",
+		Toast.makeText(this,
+				"Your screen is in dim mode. To exit dim mode, touch or press any key.",
 				Toast.LENGTH_LONG).show();
 		setContentView(blackness);
+		try {
+			getSupportActionBar().setDisplayShowHomeEnabled(false);
+		} catch (NullPointerException npe) {
+			getActionBar().setDisplayShowHomeEnabled(false);
+		}
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 		finish(); // if we're going away, really go away.
+	}
+
+	@Override
+	public void onUserInteraction() {
+		// any user interaction ends the activity.
+		finish();
 	}
 
 }
