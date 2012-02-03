d69692016b116242e860a8839606f6536cb5a10f
Brandon Edens <brandonedens@gmail.com>
2012-02-03 09:56:23 -0800

    Remove unused imports to correct checkstyle errors.

--- src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
+++ src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
@@ -1,15 +1,5 @@
 package com.androsz.electricsleepbeta.app;
 
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-
 import com.androsz.electricsleepbeta.R;
 
 public class SuggestionFragment extends LayoutFragment {
