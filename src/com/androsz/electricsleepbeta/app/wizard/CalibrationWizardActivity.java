47c8fdc056f2e36153a74cdfb691228b39b91fa4
Brandon Edens <brandonedens@gmail.com>
2012-02-14 14:42:26 -0800

    Remove unused import.

--- src/com/androsz/electricsleepbeta/app/wizard/CalibrationWizardActivity.java
+++ src/com/androsz/electricsleepbeta/app/wizard/CalibrationWizardActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
-import android.widget.TextView;
 
 import com.androsz.electricsleepbeta.R;
 import com.androsz.electricsleepbeta.app.SettingsActivity;
