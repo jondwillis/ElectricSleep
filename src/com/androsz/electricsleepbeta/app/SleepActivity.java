7b1fae70ed53c578506367770e05ea3478dd4cac
Brandon Edens <brandonedens@gmail.com>
2012-02-16 17:32:49 -0800

    Remove unused imports.

--- src/com/androsz/electricsleepbeta/app/SleepActivity.java
+++ src/com/androsz/electricsleepbeta/app/SleepActivity.java
@@ -17,8 +17,6 @@ import android.support.v4.view.Menu;
 import android.support.v4.view.MenuItem;
 import android.text.format.DateFormat;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ScrollView;
 import android.widget.Toast;
