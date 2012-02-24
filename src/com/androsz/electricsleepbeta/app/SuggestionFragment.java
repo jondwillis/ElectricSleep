96b1b0b5d10be67492ef680e6a8a6d731fe1388f
jon <jondwillis@gmail.com>
2012-02-24 14:02:20 -0500

    Added dummy action (opens myzeo.com) to suggestion buttons.

--- src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
+++ src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
@@ -1,8 +1,44 @@
 package com.androsz.electricsleepbeta.app;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import com.androsz.electricsleepbeta.R;
 
 public class SuggestionFragment extends LayoutFragment {
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		Activity a = getActivity();
+		a.findViewById(R.id.btn_sleep_aids).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+					    startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://myzeo.com/")));
+					}
+				});
+		a.findViewById(R.id.btn_advice).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+                        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://myzeo.com/")));
+					}
+				});
+		a.findViewById(R.id.btn_consultation).setOnClickListener(
+				new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+                        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://myzeo.com/")));
+					}
+				});
+	}
 
 	@Override
 	public int getLayoutResourceId() {
