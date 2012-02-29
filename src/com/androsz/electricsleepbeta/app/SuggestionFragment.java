f570ab7216ed80c57f2cc9ab7c645a73181b3aca
jon <jondwillis@gmail.com>
2012-02-28 20:50:06 -0500

    fragment_suggestion now centers the drawable of each "button" in landscape mode.

--- src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
+++ src/com/androsz/electricsleepbeta/app/SuggestionFragment.java
@@ -6,36 +6,61 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 
 import com.androsz.electricsleepbeta.R;
 
 public class SuggestionFragment extends LayoutFragment {
+
+	private void setOnClickForEntireViewGroup(View v,
+			BubbleDownOnClickListener l) {
+		v.setOnClickListener(l);
+
+		if (v instanceof ViewGroup) {
+			for (int i = 0; i < ((ViewGroup) v).getChildCount(); i++) {
+				((ViewGroup) v).getChildAt(i).setOnClickListener(l);
+			}
+		}
+	}
+
+	class BubbleDownOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			if (v instanceof ViewGroup) {
+				for (int i = 0; i < ((ViewGroup) v).getChildCount(); i++) {
+					((ViewGroup) v).getChildAt(i).performClick();
+				}
+			}
+		}
+	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
 		Activity a = getActivity();
-		a.findViewById(R.id.btn_sleep_store).setOnClickListener(
-				new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-					    startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://www.myzeo.com/sleep/shop/zeo-recommended.html")));
-					}
-				});
-		a.findViewById(R.id.btn_expert_advice).setOnClickListener(
-				new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-                        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://myzeo.com/")));
-					}
-				});
-		a.findViewById(R.id.btn_consultation).setOnClickListener(
-				new OnClickListener() {
 
+		setOnClickForEntireViewGroup(a.findViewById(R.id.btn_sleep_store),
+				new BubbleDownOnClickListener() {
 					@Override
 					public void onClick(View v) {
-                        startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://myzeo.com/")));
+						super.onClick(v);
+						startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri
+								.parse("http://www.myzeo.com/sleep/shop/zeo-recommended.html")));
+					}
+				});
+		setOnClickForEntireViewGroup(a.findViewById(R.id.btn_expert_advice),
+				new BubbleDownOnClickListener() {
+					@Override
+					public void onClick(View v) {
+						super.onClick(v);
+					}
+				});
+		setOnClickForEntireViewGroup(a.findViewById(R.id.btn_consultation),
+				new BubbleDownOnClickListener() {
+					@Override
+					public void onClick(View v) {
+						super.onClick(v);
 					}
 				});
 	}
