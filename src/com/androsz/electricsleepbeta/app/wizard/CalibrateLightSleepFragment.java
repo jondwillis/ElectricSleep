27352b316a7c12c2c9c9aeaa3afa8be8f953079e
Brandon Edens <brandonedens@gmail.com>
2012-03-05 13:41:45 -0800

    Remove tab characters from file.

--- src/com/androsz/electricsleepbeta/app/wizard/CalibrateLightSleepFragment.java
+++ src/com/androsz/electricsleepbeta/app/wizard/CalibrateLightSleepFragment.java
@@ -24,96 +24,96 @@ import com.androsz.electricsleepbeta.widget.SleepChart;
 import com.androsz.electricsleepbeta.widget.VerticalSeekBar;
 
 public class CalibrateLightSleepFragment extends LayoutFragment implements
-		Calibrator {
+        Calibrator {
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-		// if (savedInstanceState == null) {
-		sleepChart = (SleepChart) getActivity().findViewById(
-				R.id.calibration_sleep_chart);
-		// } else {
-		// sleepChart = (SleepChart)
-		// savedInstanceState.getParcelable(SLEEP_CHART);
-		// }
+        // if (savedInstanceState == null) {
+        sleepChart = (SleepChart) getActivity().findViewById(
+                R.id.calibration_sleep_chart);
+        // } else {
+        // sleepChart = (SleepChart)
+        // savedInstanceState.getParcelable(SLEEP_CHART);
+        // }
 
-		final VerticalSeekBar seekBar = (VerticalSeekBar) getActivity()
-				.findViewById(R.id.calibration_level_seekbar);
-		seekBar.setMax((int) SettingsActivity.MAX_ALARM_SENSITIVITY);
-		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+        final VerticalSeekBar seekBar = (VerticalSeekBar) getActivity()
+                .findViewById(R.id.calibration_level_seekbar);
+        seekBar.setMax((int) SettingsActivity.MAX_ALARM_SENSITIVITY);
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
-			@Override
-			public void onProgressChanged(final SeekBar seekBar,
-					final int progress, final boolean fromUser) {
-				if (fromUser) {
-					sleepChart.setCalibrationLevel(progress
-							/ DecimalSeekBar.PRECISION);
-				}
-			}
+            @Override
+            public void onProgressChanged(final SeekBar seekBar,
+                    final int progress, final boolean fromUser) {
+                if (fromUser) {
+                    sleepChart.setCalibrationLevel(progress
+                            / DecimalSeekBar.PRECISION);
+                }
+            }
 
-			@Override
-			public void onStartTrackingTouch(final SeekBar seekBar) {
-			}
+            @Override
+            public void onStartTrackingTouch(final SeekBar seekBar) {
+            }
 
-			@Override
-			public void onStopTrackingTouch(final SeekBar seekBar) {
-			}
-		});
+            @Override
+            public void onStopTrackingTouch(final SeekBar seekBar) {
+            }
+        });
 
-		sleepChart.clear();
-		sleepChart.setVisibility(View.INVISIBLE);
-		getActivity().findViewById(R.id.calibration_level_seekbar)
-				.setVisibility(View.INVISIBLE);
-		getActivity().findViewById(R.id.warming_up_text).setVisibility(
-				View.VISIBLE);
-		sleepChart
-				.setCalibrationLevel(SettingsActivity.DEFAULT_ALARM_SENSITIVITY);
+        sleepChart.clear();
+        sleepChart.setVisibility(View.INVISIBLE);
+        getActivity().findViewById(R.id.calibration_level_seekbar)
+                .setVisibility(View.INVISIBLE);
+        getActivity().findViewById(R.id.warming_up_text).setVisibility(
+                View.VISIBLE);
+        sleepChart
+                .setCalibrationLevel(SettingsActivity.DEFAULT_ALARM_SENSITIVITY);
 
-		getActivity().getWindow().addFlags(
-				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	}
+        getActivity().getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
 
-	private static final String SLEEP_CHART = "sleepChart";
+    private static final String SLEEP_CHART = "sleepChart";
 
-	SleepChart sleepChart;
+    SleepChart sleepChart;
 
-	private final BroadcastReceiver syncChartReceiver = new BroadcastReceiver() {
-		@SuppressWarnings("unchecked")
-		@Override
-		public void onReceive(final Context context, final Intent intent) {
+    private final BroadcastReceiver syncChartReceiver = new BroadcastReceiver() {
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
 
-			// throw new Exception("UHHHH");
-			sleepChart = (SleepChart) getActivity().findViewById(
-					R.id.calibration_sleep_chart);
+            // throw new Exception("UHHHH");
+            sleepChart = (SleepChart) getActivity().findViewById(
+                    R.id.calibration_sleep_chart);
 
-			List<PointD> points = (List<PointD>) intent
-					.getSerializableExtra(SleepMonitoringService.SLEEP_DATA);
-			for (PointD point : points) {
-				sleepChart.addPoint(point.x, point.y);
-			}
+            List<PointD> points = (List<PointD>) intent
+                    .getSerializableExtra(SleepMonitoringService.SLEEP_DATA);
+            for (PointD point : points) {
+                sleepChart.addPoint(point.x, point.y);
+            }
 
-			sleepChart.reconfigure();
-			sleepChart.repaint();
-		}
-	};
+            sleepChart.reconfigure();
+            sleepChart.repaint();
+        }
+    };
 
-	private final BroadcastReceiver updateChartReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(final Context context, final Intent intent) {
-			/*
-			 * CalibrateAlarmActivity.this .setResult( CALIBRATION_SUCCEEDED,
-			 * new Intent().putExtra("y", sleepChart.getCalibrationLevel()));
-			 */
-			getActivity().findViewById(R.id.calibration_sleep_chart)
-					.setVisibility(View.VISIBLE);
-			getActivity().findViewById(R.id.calibration_level_seekbar)
-					.setVisibility(View.VISIBLE);
-			getActivity().findViewById(R.id.warming_up_text).setVisibility(
-					View.GONE);
-			if (sleepChart != null) {
-				final VerticalSeekBar seekBar = (VerticalSeekBar) getActivity()
-						.findViewById(R.id.calibration_level_seekbar);
+    private final BroadcastReceiver updateChartReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            /*
+             * CalibrateAlarmActivity.this .setResult( CALIBRATION_SUCCEEDED,
+             * new Intent().putExtra("y", sleepChart.getCalibrationLevel()));
+             */
+            getActivity().findViewById(R.id.calibration_sleep_chart)
+                    .setVisibility(View.VISIBLE);
+            getActivity().findViewById(R.id.calibration_level_seekbar)
+                    .setVisibility(View.VISIBLE);
+            getActivity().findViewById(R.id.warming_up_text).setVisibility(
+                    View.GONE);
+            if (sleepChart != null) {
+                final VerticalSeekBar seekBar = (VerticalSeekBar) getActivity()
+                        .findViewById(R.id.calibration_level_seekbar);
                 if (sleepChart.hasCalibrationLevel()) {
                     seekBar.setProgress((float) sleepChart.getCalibrationLevel());
                     sleepChart.sync(intent.getDoubleExtra(
@@ -122,45 +122,45 @@ public class CalibrateLightSleepFragment extends LayoutFragment implements
                                     sleepChart.getCalibrationLevel());
                 }
             }
-		}
-	};
+        }
+    };
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		getActivity().registerReceiver(updateChartReceiver,
-				new IntentFilter(SleepActivity.UPDATE_CHART));
-		getActivity().registerReceiver(syncChartReceiver,
-				new IntentFilter(SleepActivity.SYNC_CHART));
-		getActivity().sendBroadcast(
-				new Intent(SleepMonitoringService.POKE_SYNC_CHART));
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(updateChartReceiver,
+                new IntentFilter(SleepActivity.UPDATE_CHART));
+        getActivity().registerReceiver(syncChartReceiver,
+                new IntentFilter(SleepActivity.SYNC_CHART));
+        getActivity().sendBroadcast(
+                new Intent(SleepMonitoringService.POKE_SYNC_CHART));
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		stopCalibration(getActivity());
-		getActivity().unregisterReceiver(updateChartReceiver);
-		getActivity().unregisterReceiver(syncChartReceiver);
-	}
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopCalibration(getActivity());
+        getActivity().unregisterReceiver(updateChartReceiver);
+        getActivity().unregisterReceiver(syncChartReceiver);
+    }
 
-	@Override
-	public int getLayoutResourceId() {
-		return R.layout.wizard_calibration_lightsleep;
-	}
+    @Override
+    public int getLayoutResourceId() {
+        return R.layout.wizard_calibration_lightsleep;
+    }
 
-	@Override
-	public void startCalibration(Context context) {
-		final Intent i = new Intent(context, SleepMonitoringService.class);
-		context.stopService(i);
-		i.putExtra("testModeRate",
-				CalibrationWizardActivity.LIGHT_SLEEP_CALIBRATION_INTERVAL);
-		i.putExtra("alarm", SettingsActivity.MAX_ALARM_SENSITIVITY);
-		context.startService(i);
-	}
+    @Override
+    public void startCalibration(Context context) {
+        final Intent i = new Intent(context, SleepMonitoringService.class);
+        context.stopService(i);
+        i.putExtra("testModeRate",
+                CalibrationWizardActivity.LIGHT_SLEEP_CALIBRATION_INTERVAL);
+        i.putExtra("alarm", SettingsActivity.MAX_ALARM_SENSITIVITY);
+        context.startService(i);
+    }
 
-	@Override
-	public void stopCalibration(Context context) {
-		context.stopService(new Intent(context, SleepMonitoringService.class));
-	}
+    @Override
+    public void stopCalibration(Context context) {
+        context.stopService(new Intent(context, SleepMonitoringService.class));
+    }
 }
