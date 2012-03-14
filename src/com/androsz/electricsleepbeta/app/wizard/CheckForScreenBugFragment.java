909584dc0643583ca2fd97a9b0b15e57b56134e9
Brandon Edens <brandonedens@gmail.com>
2012-03-14 16:07:08 -0700

    Add page flipper used to indicate to user when the standby test has completed.

--- src/com/androsz/electricsleepbeta/app/wizard/CheckForScreenBugFragment.java
+++ src/com/androsz/electricsleepbeta/app/wizard/CheckForScreenBugFragment.java
@@ -2,30 +2,50 @@ package com.androsz.electricsleepbeta.app.wizard;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.androsz.electricsleepbeta.R;
 import com.androsz.electricsleepbeta.app.CheckForScreenBugAccelerometerService;
+import com.androsz.electricsleepbeta.widget.SafeViewFlipper;
 
 public class CheckForScreenBugFragment extends Calibrator {
+
+    private static final int FLIPPER_INSTRUCTIONS = 0;
+    private static final int FLIPPER_RESULTS = 1;
+
+    private SafeViewFlipper mFlipper;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+        mFlipper = (SafeViewFlipper) root.findViewById(R.id.content_flipper);
+        return root;
+    }
+
 
 	@Override
 	public void onResume() {
 		super.onResume();
 
-		if (canBegin) {
+        if (canBegin) {
 			if (calibrationStateListener != null) {
-				calibrationStateListener.onCalibrationComplete(true);
+                mFlipper.setDisplayedChild(FLIPPER_RESULTS);
+                calibrationStateListener.onCalibrationComplete(true);
 			}
 		}
 	}
 
 	/*
 	 * @Override public void onPause() { super.onPause();
-	 * 
+	 *
 	 * final Intent i = new Intent(getActivity(),
 	 * CheckForScreenBugAccelerometerService.class);
-	 * 
+	 *
 	 * // this replaces the need for broadcast receivers. // the service updates
 	 * BUG_PRESENT_INTENT, THEN our activity is // alerted. if
 	 * (BUG_PRESENT_INTENT != null) { stopService(i); BUG_PRESENT_INTENT = null;
@@ -43,7 +63,7 @@ public class CheckForScreenBugFragment extends Calibrator {
 	@Override
 	public void startCalibration(Activity context) {
 		if (!canBegin) {
-			canBegin = true;
+            canBegin = true;
 
 			final Intent i = new Intent(context,
 					CheckForScreenBugAccelerometerService.class);
@@ -56,7 +76,7 @@ public class CheckForScreenBugFragment extends Calibrator {
 
 	@Override
 	public void stopCalibration(Activity context) {
-		if (canBegin) {
+        if (canBegin) {
 			canBegin = false;
 			final Intent i = new Intent(context,
 					CheckForScreenBugAccelerometerService.class);
