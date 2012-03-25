e5ca87bec854ac708eb43887886ffc185f0273af
jon <jondwillis@gmail.com>
2012-03-24 21:33:37 -0400

    disableKeyguard() is now called when screen is turned on by screen test.

--- src/com/androsz/electricsleepbeta/app/wizard/CheckForScreenBugAccelerometerService.java
+++ src/com/androsz/electricsleepbeta/app/wizard/CheckForScreenBugAccelerometerService.java
@@ -1,5 +1,6 @@
 package com.androsz.electricsleepbeta.app.wizard;
 
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -21,169 +22,174 @@ import com.androsz.electricsleepbeta.app.SettingsActivity;
 import com.androsz.electricsleepbeta.util.WakeLockManager;
 
 public class CheckForScreenBugAccelerometerService extends Service implements
-		SensorEventListener {
+        SensorEventListener {
 
     public static final String ACTION_BUG_PRESENT = "bug_present";
     public static final String ACTION_BUG_NOT_PRESENT = "bug_not_present";
 
     private final class ScreenReceiver extends BroadcastReceiver {
 
-		@Override
-		public void onReceive(final Context context, final Intent intent) {
-			if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-				serviceHandler.postDelayed(setScreenIsOffRunnable, 6666);
-				serviceHandler.postDelayed(turnScreenOnFallbackRunnable, 12000);
-			}
-		}
-	}
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                serviceHandler.postDelayed(setScreenIsOffRunnable, 6666);
+                serviceHandler.postDelayed(turnScreenOnFallbackRunnable, 12000);
+            }
+        }
+    }
 
-	private static final int NOTIFICATION_ID = 0x1337b;
+    private static final int NOTIFICATION_ID = 0x1337b;
 
-	private boolean bugPresent = true;
-	boolean didNotTurnScreenOn = true;
-	private PowerManager powerManager;
-	private boolean screenIsOff = false;
+    private boolean bugPresent = true;
+    boolean didNotTurnScreenOn = true;
+    private PowerManager powerManager;
+    private boolean screenIsOff = false;
 
-	private ScreenReceiver screenOnOffReceiver;
-	Handler serviceHandler = new Handler();
+    private ScreenReceiver screenOnOffReceiver;
+    Handler serviceHandler = new Handler();
 
-	Runnable setScreenIsOffRunnable = new Runnable() {
-		@Override
-		public void run() {
-			screenIsOff = true;
-			Log.d("ES", "setScreenIsOffRunnable");
-		}
-	};
+    Runnable setScreenIsOffRunnable = new Runnable() {
+        @Override
+        public void run() {
+            screenIsOff = true;
+            Log.d("ES", "setScreenIsOffRunnable");
+        }
+    };
 
-	Runnable turnScreenOnFallbackRunnable = new Runnable() {
-		@Override
-		public void run() {
+    Runnable turnScreenOnFallbackRunnable = new Runnable() {
+        @Override
+        public void run() {
 
-			Log.d("ES", "turnScreenOnFallbackRunnable");
-			if (bugPresent) {
-				getSharedPreferences(SettingsActivity.PREFERENCES, 0)
-						.edit()
-						.putBoolean(getString(R.string.pref_force_screen),
-								bugPresent).commit();
-				Log.d("ES", "bug is present");
-	            CheckForScreenBugFragment.SCREEN_BUG_STATE = CheckForScreenBugFragment.SCREEN_BUG_PRESENT;
+            Log.d("ES", "turnScreenOnFallbackRunnable");
+            if (bugPresent) {
+                getSharedPreferences(SettingsActivity.PREFERENCES, 0)
+                        .edit()
+                        .putBoolean(getString(R.string.pref_force_screen),
+                                bugPresent).commit();
+                Log.d("ES", "bug is present");
+                CheckForScreenBugFragment.SCREEN_BUG_STATE = CheckForScreenBugFragment.SCREEN_BUG_PRESENT;
             }
             turnScreenOnAndStopSelf();
-		}
-	};
+        }
+    };
 
-	private Notification createServiceNotification() {
-		final int icon = R.drawable.ic_stat_notify_track;
-		final CharSequence tickerText = getString(R.string.notification_screenbug_ticker);
-		final long when = System.currentTimeMillis();
+    private Notification createServiceNotification() {
+        final int icon = R.drawable.ic_stat_notify_track;
+        final CharSequence tickerText = getString(R.string.notification_screenbug_ticker);
+        final long when = System.currentTimeMillis();
 
-		final Notification notification = new Notification(icon, tickerText,
-				when);
+        final Notification notification = new Notification(icon, tickerText,
+                when);
 
-		notification.flags = Notification.FLAG_ONGOING_EVENT;
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
 
-		final CharSequence contentTitle = getString(R.string.notification_screenbug_ticker);
-		final CharSequence contentText = getString(R.string.notification_screenbug_ticker);
-		Intent notificationIntent = new Intent();
+        final CharSequence contentTitle = getString(R.string.notification_screenbug_ticker);
+        final CharSequence contentText = getString(R.string.notification_screenbug_ticker);
+        Intent notificationIntent = new Intent();
 
-		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
-				| Intent.FLAG_ACTIVITY_NEW_TASK);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
 
-		final PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-				notificationIntent, 0);
+        final PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
 
-		notification.setLatestEventInfo(this, contentTitle, contentText,
-				contentIntent);
+        notification.setLatestEventInfo(this, contentTitle, contentText,
+                contentIntent);
 
-		return notification;
-	}
+        return notification;
+    }
 
-	@Override
-	public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
-		// Not used.
-	}
+    @Override
+    public void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+        // Not used.
+    }
 
-	@Override
-	public IBinder onBind(final Intent intent) {
-		// Not used
-		return null;
-	}
+    @Override
+    public IBinder onBind(final Intent intent) {
+        // Not used
+        return null;
+    }
 
-	@Override
-	public void onDestroy() {
+    @Override
+    public void onDestroy() {
 
-		unregisterAccelerometerListener();
+        unregisterAccelerometerListener();
 
-		unregisterReceiver(screenOnOffReceiver);
+        unregisterReceiver(screenOnOffReceiver);
 
-		serviceHandler.removeCallbacks(setScreenIsOffRunnable);
-		serviceHandler.removeCallbacks(turnScreenOnFallbackRunnable);
+        serviceHandler.removeCallbacks(setScreenIsOffRunnable);
+        serviceHandler.removeCallbacks(turnScreenOnFallbackRunnable);
 
-		// check here so that certain devices keep their screen on for at least
-		// 5 seconds (from turnScreenOn)
-		if (didNotTurnScreenOn) {
-			WakeLockManager.release("screenBugPartial");
-			WakeLockManager.release("screenBugDim");
-		}
-		super.onDestroy();
-		Log.d("ES", "DESTROYED");
-	}
+        // check here so that certain devices keep their screen on for at least
+        // 5 seconds (from turnScreenOn)
+        if (didNotTurnScreenOn) {
+            WakeLockManager.release("screenBugPartial");
+            WakeLockManager.release("screenBugDim");
+        }
+        super.onDestroy();
+        Log.d("ES", "DESTROYED");
+    }
 
-	@Override
-	public void onSensorChanged(final SensorEvent event) {
-		if (!powerManager.isScreenOn() && screenIsOff && bugPresent) {
-			getSharedPreferences(SettingsActivity.PREFERENCES, 0).edit()
-					.putBoolean(getString(R.string.pref_force_screen), false)
-					.commit();
-			Log.d("ES", "bug not present.");
-	        CheckForScreenBugFragment.SCREEN_BUG_STATE = CheckForScreenBugFragment.SCREEN_BUG_NOT_PRESENT;
+    @Override
+    public void onSensorChanged(final SensorEvent event) {
+        if (!powerManager.isScreenOn() && screenIsOff && bugPresent) {
+            getSharedPreferences(SettingsActivity.PREFERENCES, 0).edit()
+                    .putBoolean(getString(R.string.pref_force_screen), false)
+                    .commit();
+            Log.d("ES", "bug not present.");
+            CheckForScreenBugFragment.SCREEN_BUG_STATE = CheckForScreenBugFragment.SCREEN_BUG_NOT_PRESENT;
             turnScreenOnAndStopSelf();
-		}
-	}
+        }
+    }
 
-	@Override
-	public int onStartCommand(final Intent intent, final int flags,
-			final int startId) {
-		if (startId == 1) {
-			Log.d("ES", "onStartCommand1");
-			bugPresent = true;
-			screenIsOff = false;
-			final IntentFilter filter = new IntentFilter(
-					Intent.ACTION_SCREEN_ON);
-			filter.addAction(Intent.ACTION_SCREEN_OFF);
-			screenOnOffReceiver = new ScreenReceiver();
-			registerReceiver(screenOnOffReceiver, filter);
-			powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
-			startForeground(NOTIFICATION_ID, createServiceNotification());
-			WakeLockManager.acquire(this, "screenBugPartial",
-					PowerManager.PARTIAL_WAKE_LOCK);
-			registerAccelerometerListener();
-		}
-		return startId;
-	}
+    @Override
+    public int onStartCommand(final Intent intent, final int flags,
+            final int startId) {
+        if (startId == 1) {
+            Log.d("ES", "onStartCommand1");
+            bugPresent = true;
+            screenIsOff = false;
+            final IntentFilter filter = new IntentFilter(
+                    Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            screenOnOffReceiver = new ScreenReceiver();
+            registerReceiver(screenOnOffReceiver, filter);
+            powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            startForeground(NOTIFICATION_ID, createServiceNotification());
+            WakeLockManager.acquire(this, "screenBugPartial",
+                    PowerManager.PARTIAL_WAKE_LOCK);
+            registerAccelerometerListener();
+        }
+        return startId;
+    }
 
-	private void registerAccelerometerListener() {
-		Log.d("ES", "registerAccelerometerListener");
-		final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    private void registerAccelerometerListener() {
+        Log.d("ES", "registerAccelerometerListener");
+        final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-		sensorManager.registerListener(this,
-				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				SensorManager.SENSOR_DELAY_FASTEST);
-	}
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
 
-	private void turnScreenOnAndStopSelf() {
-		Log.d("ES", "turnScreenOnAndStopSelf");
-		didNotTurnScreenOn = false;
-		WakeLockManager.acquire(this, "screenBugDim",
-				PowerManager.SCREEN_DIM_WAKE_LOCK
-						| PowerManager.ACQUIRE_CAUSES_WAKEUP
-						| PowerManager.ON_AFTER_RELEASE, 5000);
-		stopSelf();
-	}
+    private void turnScreenOnAndStopSelf() {
+        Log.d("ES", "turnScreenOnAndStopSelf");
+        didNotTurnScreenOn = false;
+        WakeLockManager.acquire(this, "screenBugDim",
+                PowerManager.SCREEN_DIM_WAKE_LOCK
+                        | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                        | PowerManager.ON_AFTER_RELEASE, 5000);
 
-	private void unregisterAccelerometerListener() {
-		Log.d("ES", "unregisterAccelerometerListener");
-		final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		sensorManager.unregisterListener(this);
-	}
+        // try to disable lockscreen
+        ((KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE))
+                .newKeyguardLock("ES").disableKeyguard();
+
+        stopSelf();
+    }
+
+    private void unregisterAccelerometerListener() {
+        Log.d("ES", "unregisterAccelerometerListener");
+        final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.unregisterListener(this);
+    }
 }
